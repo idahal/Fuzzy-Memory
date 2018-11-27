4fb26999576e0cf7c1ce9369c98683830ca92313
Ida Hallner <bee_ida@hotmail.com>
2018-11-27 09:31:05 +0100

    add make match and flip

---
+++ script.js
@@ -0,0 +1,49 @@
+const cards = document.querySelectorAll('.memory-card');
+
+function resetBoard() {
+  [hasFlipCard, stopFlip] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+let hasFlipCard = false;
+let stopFlip = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (stopFlip) return;
+  if (this === firstCard) return;
+  this.classList.add('flip');
+
+
+if (!hasFlipCard) {
+  hasFlipCard = true;
+  firstCard = this;
+  return;
+}
+
+secondCard = this;
+
+checkForPair();
+}
+
+function checkForPair() {
+  let ifPair = firstCard.dataset.pony === secondCard.dataset.pony;
+  ifPair ?  disableCards(): unFlipCards();
+  }
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+   secondCard.removeEventListener('click', flipCard);
+
+   resetBoard();
+}
+
+function unFlipCards () {
+  stopFlip = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    // stopFlip = false;
+    resetBoard();
+  }, 1700);
+}
