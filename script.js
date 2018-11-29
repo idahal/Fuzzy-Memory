6d1add69d6ec0a3a7b3311752f9c5b8ea71ed51b
Ida Hallner <bee_ida@hotmail.com>
2018-11-29 09:07:41 +0100

    add replay function

--- script.js
+++ script.js
@@ -16,19 +16,25 @@ const createCard = (id, image) => {
     <img class="item-back" src="${image}" alt="">
   </div>`
 }
-const duplicated = [...memorycards, ...memorycards]
-duplicated.forEach(function(card) {
+const duplicated = [...memorycards, ...memorycards];
+//moved from foreach
 const memorygame = document.querySelector('.memory-game');
-memorygame.innerHTML +=createCard(card.id, card.image);
+duplicated.forEach(function(card) {
+  memorygame.innerHTML +=createCard(card.id, card.image);
 });
-
-
 
 const cards = document.querySelectorAll('.memory-card');
 
 function resetBoard() {
   [hasFlipCard, stopFlip] = [false, false];
   [firstCard, secondCard] = [null, null];
+
+  //create replay button after game finish. If all cards is = cards who been flipped
+  if(cards.length === memorygame.querySelectorAll('.flip').length){
+    console.log('All is flipped!');
+    memorygame.insertAdjacentHTML('beforeend','<div class="reset"><button class="reset-all"><h3>You won!</h3> <br> Press if you want to play again</button></div>');
+    document.querySelector('.reset-all').addEventListener('click', resetAll);
+  }
 }
 let hasFlipCard = false;
 let stopFlip = false;
@@ -71,15 +77,29 @@ function unFlipCards () {
     secondCard.classList.remove('flip');
     // stopFlip = false;
     resetBoard();
-  }, 1700);
+  }, 1600);
 }
 
+//when game won, remove flip, add a click and shuffle
+function resetAll () {
+  memorygame.removeChild(memorygame.querySelector('.reset'));
+  setTimeout(() => {
+    cards.forEach(card => {
+      card.classList.remove('flip');
+      card.addEventListener('click', flipCard);
+    });
+    shuffle();
+  }, 300);
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-(function shuffle() {
+//if reset, shuffle function
+function shuffle() {
   cards.forEach(card=> {
     let randomPos = Math.floor(Math.random() * 16);
     card.style.order = randomPos;
   })
-})();
+};
+
+shuffle();
